75453ba43dad02cf536a14e02d49af035ba32967
MSGuy01 <47047931+MSGuy01@users.noreply.github.com>
2022-03-07 14:44:25 -0800

    Finished solution algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,59 @@
 import java.util.Arrays;
-import java.util.Random;
 public class Main {
-    static Random r = new Random();
+    static int[][] sudoku = {
+            {0,9,1,0,0,4,8,0,0},
+            {0,0,0,1,0,0,0,9,0},
+            {5,0,0,0,6,0,1,0,2},
+            {0,6,0,0,0,1,3,0,0},
+            {4,0,0,0,7,0,0,0,1},
+            {0,0,9,2,0,0,0,5,0},
+            {9,0,4,0,3,0,0,0,8},
+            {0,3,0,0,0,8,0,0,0},
+            {0,0,8,6,0,0,2,4,0}
+    };
     public static void main(String[] args){
-      int[][] sudoku = {
-        {0,9,1,0,0,4,8,0,0},
-        {0,0,0,1,0,0,0,9,0},
-        {5,0,0,0,6,0,1,0,2},
-        {0,6,0,0,0,1,3,0,0},
-        {4,0,0,0,7,0,0,0,1},
-        {0,0,9,2,0,0,0,5,0},
-        {9,0,4,0,3,0,0,0,8},
-        {0,3,0,0,0,8,0,0,0},
-        {0,0,8,6,0,0,2,4,0}
-      };
-      //System.out.println(Arrays.toString(box(sudoku,3, 3)));
       int timesRun = 0;
       int[][][] prevPossibilities = new int[9][9][9];
+      boolean stop = false;
+      //TODO:ADD GUI
+      //TODO:REMOVE NON-BACKTRACK ALGORITHM
       while (!check(sudoku)) {
           timesRun++;
-          System.out.println(timesRun);
           int[][][] findPossibilities = find(sudoku);
-          if (findPossibilities == prevPossibilities) {
-              System.out.println("BRUH");
-              break;
-          }
           for (int i = 0; i < findPossibilities.length; i++) {
               for (int j = 0; j < findPossibilities[i].length; j++) {
                   int p = 0;
                   for (int k = 0; k < findPossibilities[i][j].length; k++) {
                       if (findPossibilities[i][j][k] != 0) {
                           p++;
-                          System.out.print(findPossibilities[i][j][k] + ",");
                       }
                   }
                   if (p == 1) {
                       sudoku[i][j] = findPossibilities[i][j][0];
                   }
-                  System.out.println("\n");
               }
           }
-          boolean stop = true;
+          stop = true;
           for (int i = 0; i < prevPossibilities.length; i++) {
               for (int j = 0; j < prevPossibilities[i].length; j++) {
                   for (int k = 0; k < prevPossibilities[i][j].length; k++) {
                       if (prevPossibilities[i][j][k] != findPossibilities[i][j][k]) {
                           stop = false;
-                          //System.out.println("BRUH MOMENT: " + prevPossibilities[i][j][k] + " || " + findPossibilities[i][j][k]);
+                          break;
                       }
                   }
               }
           }
           if (stop) {
-              System.out.println(backtrack(0,-1, sudoku));
+              System.out.println("Backtrack necessary");
+              backtrack(0,-1);
               break;
           }
           prevPossibilities = findPossibilities;
-          System.out.println("************************************");
       }
-      System.out.println("Done! Times run: " + timesRun);
+      if (!stop) {
+          System.out.println("Done! Times run: " + timesRun);
+      }
       System.out.println(Arrays.deepToString(sudoku));
     }
     private static int[] box(int[][] sudoku, int row, int column) {
@@ -76,9 +71,31 @@ public class Main {
         }
         return finalArr;
     }
-    private static boolean backtrack(int r, int c, int[][] sudoku) {
-        //DO THIS
-        return false;
+    private static boolean backtrack(int r, int c) {
+        c++;
+        if (c > 8) {
+            c = 0;
+            r++;
+            if (r >8) {
+                return true;
+            }
+        }
+        if (sudoku[r][c] != 0) {
+            return backtrack(r,c);
+        }
+        else {
+            for (int i = 1; i < 10; i++) {
+                //if something doesn't work (reaches a "dead end"), it'll return false, ignore those solutions, and go back to where it was when it started
+                if (works(i, r, c, sudoku)) {
+                    sudoku[r][c] = i;
+                    if (backtrack(r, c)) {
+                        return true;
+                    }
+                }
+            }
+            sudoku[r][c] = 0;
+            return false;
+        }
     }
     private static int[][][] find(int[][] sudoku) {
         int[][][] finalArr = new int[9][9][9];
@@ -101,8 +118,8 @@ public class Main {
         return finalArr;
     }
     private static boolean isIn(int num, int[] arr) {
-        for (int i : arr) {
-            if (i == num) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
                 return true;
             }
         }
